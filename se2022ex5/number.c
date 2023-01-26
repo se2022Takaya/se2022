2557c287bb7da21aa902faeac3f0e3700ef540ae
takaya <r202103304nu@jindai.jp>
2023-01-26 23:32:24 +0900

    sec ex version2

--- se2022ex5/number.c
+++ se2022ex5/number.c
@@ -3,9 +3,11 @@
 int main(void){
     int x = 3;
     int y = 10;
+    int z = x*x;
 
     printf("y - x = %d", y - x);
     printf("y + x = %d", y + x);
+    printf("z = %d", z);
 
     return 0;
 }
