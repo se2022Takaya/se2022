dbbd690e9384c7d5da9f257c872a5a5a1282147e
takaya <r202103304nu@jindai.jp>
2023-01-26 23:39:11 +0900

    thi ex version2

--- se2022ex5/forloop.c
+++ se2022ex5/forloop.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(void){
-    for(int i=0;i<5;i++){
-        printf("i is cheper than 5");
+    for(int i=0;i<6;i++){
+        if(i == 5){
+            printf("i is just 5\n");
+        }
+        printf("i is smaller than 5\n");
     }
+    
     return 0;
 }
